dca79cb5c602bcc56f20f91ffd1cb48349906b29
Ken Hancock <Ken_Hancock@meei.harvard.edu>
2025-10-16 12:50:12 -0400

    adding help in different formats

---
+++ Source/help/js/settings.js
@@ -0,0 +1,9 @@
+﻿var whSettings = {
+	highlightSearchWords : true,
+	highlightSearchWordsSingleColor : false,
+	tocOnlyExpandSingleHeading : false,
+	tocAutoExpandOnActivate : false,
+	tocAtimateExpandingCollapsingDuration : 200,
+	indexFile : 'index.htm',
+	unknownFile : 'unknown.htm'
+}
